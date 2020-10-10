08a103ddb2f7a323945b6d810c4aaf2ecb118c3d
Douglas Marques <douglas.bmarques@hotmail.com>
2020-10-10 13:56:44 -0300

    Procfile change

--- api/api.py
+++ api/api.py
@@ -7,5 +7,5 @@ from routes import *
 api.config['DEBUG'] = True
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    api.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    api.run(host='0.0.0.0', port=5000)
